44990f7f72c5be402d5ff7abe63db49278451f8c
cmmadnat <cmmadnat@gmail.com>
2019-08-02 17:40:34 +0700

    configure authorization server

--- src/main/kotlin/com/dhi/restapi/RestApiApplication.kt
+++ src/main/kotlin/com/dhi/restapi/RestApiApplication.kt
@@ -2,8 +2,10 @@ package com.dhi.restapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
 
 @SpringBootApplication
+@EnableAuthorizationServer
 class RestApiApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/dhi/restapi/SecurityConfiguration.kt
+++ src/main/kotlin/com/dhi/restapi/SecurityConfiguration.kt
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.User
@@ -12,7 +13,14 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer
+import org.springframework.security.oauth2.core.AuthorizationGrantType
 import org.springframework.stereotype.Component
+import javax.servlet.http.HttpServletResponse
+
 
 @Configuration
 class SecurityConfiguration : WebSecurityConfigurerAdapter() {
@@ -26,14 +34,41 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
         return myUserDetailService
     }
 
+    public override fun configure(http: HttpSecurity) {
+        http.csrf().disable().exceptionHandling()
+                .authenticationEntryPoint { _, response, _ -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED) }
+                .and().authorizeRequests().antMatchers("/**").authenticated().and().httpBasic()
+    }
+
+
+    public override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.userDetailsService(myUserDetailService).passwordEncoder(passwordEncoder())
+    }
+
+    @Configuration
+    class AuthorizationServer : AuthorizationServerConfigurerAdapter() {
+        @Autowired
+        lateinit var passwordEncoder: BCryptPasswordEncoder
+
+        override fun configure(clients: ClientDetailsServiceConfigurer) {
+            clients.inMemory()
+                    .withClient("my-client").secret(passwordEncoder.encode(("secret")))
+                    .autoApprove(true).authorizedGrantTypes(AuthorizationGrantType.CLIENT_CREDENTIALS.value).scopes("user_info")
+
+        }
+
+
+        override fun configure(security: AuthorizationServerSecurityConfigurer) {
+            security.tokenKeyAccess("permitAll()")
+                    .checkTokenAccess("isAuthenticated()").allowFormAuthenticationForClients()
+        }
+    }
 }
 
 @Component
 class MyUserDetailService : UserDetailsService {
     @Autowired
     lateinit var staffRepository: StaffRepository
-    @Autowired
-    lateinit var passwordEncoder: BCryptPasswordEncoder
 
     override fun loadUserByUsername(p0: String?): UserDetails {
         if (p0 != null) {
